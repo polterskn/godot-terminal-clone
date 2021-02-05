cf4a0229ffb439fb1f59c6c91c501646946f45c7
Polter97 <hector.loyo@hotmail.com>
2021-02-05 02:45:01 -0700

    Replaced most matches with for cycles

--- scripts/terminal/commands.gd
+++ scripts/terminal/commands.gd
@@ -3,7 +3,7 @@ extends Node
 
 onready var main = get_parent()
 
-
+# Checks if command syntax is correct (if not return an error log)
 func syntaxCheck(command, option):
 	if 'cd ' in command:
 		return true
@@ -12,6 +12,7 @@ func syntaxCheck(command, option):
 	else:
 		main.exceptionOccur(1, String('>' + command.replace(" ", "") + ' ' + option))
 
+# Complex commands
 func cat(syntax):
 	if syntaxCheck(syntax, '[file]'):
 		print('cat success')
@@ -24,31 +25,16 @@ func cd(syntax):
 
 func man(syntax):
 	if syntaxCheck(syntax, '[command]') && 'm' in syntax[0]:
+			var param = ['mancat', 'mancd', 'manclear', 'mandate', 'manhelp', 'manhistory', 'manls', 'manman', 'manrun', 'manpwd']
 			var option = String(syntax.replace(" ", ""))
 			
-			match option:
-				'mancat':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/cat.txt'))
-				'mancd':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/cd.txt'))
-				'manclear':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/clear.txt'))
-				'mandate':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/date.txt'))
-				'manhelp':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/help.txt'))
-				'manhistory':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/history.txt'))
-				'manls':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/ls.txt'))
-				'manman':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/man.txt'))
-				'manrun':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/run.txt'))
-				'manpwd':
-					main.addToConsoleLog(0, main.load_file('res://media/txt/desc/pwd.txt'))
-				_:
-					main.exceptionOccur(1, String('>man ' + option.replace("man", "") + ' [nonexistent option]'))
+			for i in range(0, param.size()):
+				if option == param[i]:
+					var route = String('res://media/txt/desc/' + option + '.txt')
+					main.addToConsoleLog(0, main.load_file(route))
+					return
+					
+			main.exceptionOccur(1, String('>man ' + option.replace("man", "") + ' [nonexistent option]'))
 
 
 func run(syntax):
--- scripts/terminal/main.gd
+++ scripts/terminal/main.gd
@@ -3,12 +3,22 @@ extends Control
 
 const ROOT_DIR = '/home/user'
 const MONTHS = ['', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+const SIMPLE_COMMANDS = ['clear', 'date', 'help', 'history', 'ls', 'pwd']
 
 onready var consoleLog = $Console/console__log
 onready var consoleInput = $Console/Input/console__input
 onready var writeIndicator = $Console/Input/write__indicator
 onready var commands = $commands
 onready var anim = $anim
+
+var simpleFunctions = [
+	funcref(self, "clearTerminal"),
+	funcref(self, "printDate"),
+	funcref(self, "showHelp"),
+	funcref(self, "printHistory"),
+	funcref(self, "listContent"),
+	funcref(self, "printCurrentDir")
+]
 
 var commandHistory = []
 var historyIndex = -1
@@ -17,11 +27,11 @@ var currentDirectory
 
 var finished = false
 
-
+# Called once after the scene is created
 func _ready():
 	initTerminal()
 
-
+# Called once per frame
 func _process(_delta):
 	consoleInput.visible = finished
 	writeIndicator.visible = finished
@@ -31,7 +41,7 @@ func _process(_delta):
 	else:
 		consoleInput.placeholder_text = ''
 
-
+# Initializes the terminal
 func initTerminal():
 	var br = '\n\n'
 	var initialText = []
@@ -48,7 +58,7 @@ func initTerminal():
 	printLog(initialText, 2) #10
 	currentDirectory = ROOT_DIR
 
-
+# Gets datetime from OS and returns the full date as string
 func transformDate():
 	var date = OS.get_datetime()
 	var month
@@ -57,9 +67,10 @@ func transformDate():
 		if date.month == i:
 			month = MONTHS[i]
 	
+	# finally it returns the full date as string
 	return String(month + ' ' + String(date.day) + ', ' + String(date.year) + ' - ' + String(date.hour) + ':' + String(date.minute) + ':' + String(date.second))
 
-
+# Gets txt files and returns their content as string.
 func load_file(file):
 	var f = File.new()
 	var data
@@ -68,7 +79,7 @@ func load_file(file):
 	f.close()
 	return data
 
-
+# Animates consoleLog percent_visible property
 func printLog(_dialog, duration):
 	finished = false
 	consoleLog.percent_visible = 0
@@ -78,7 +89,7 @@ func printLog(_dialog, duration):
 	)
 	anim.start()
 
-
+# Validates whether the introduced command exists or not
 func validateCommand(command):
 	
 	consoleInput.clear()
@@ -100,33 +111,27 @@ func validateCommand(command):
 			exceptionOccur(0, command)
 			
 	else:
+			for i in range(0, SIMPLE_COMMANDS.size()):
+				if command == SIMPLE_COMMANDS[i]:
+					simpleFunctions[i].call_func()
+			
+			var exception
 			match command:
-				'clear':
-					consoleLog.bbcode_text = ''
-				'date':
-					addToConsoleLog(0, transformDate())
-				'help':
-					addToConsoleLog(0, load_file('res://media/txt/help.txt'))
-				'history':
-					printHistory()
-				'ls':
-					pass # List content
-				'pwd':
-					addToConsoleLog(0, currentDirectory)
+				'man':
+					exception = ' [command]'
+				'cat':
+					exception = ' [file]'
+				'cd':
+					exception = ' [directory]'
+				'run':
+					exception = ' [file]'
 				_:
-					match command:
-						'man':
-							exceptionOccur(1, String(command + ' [command]'))
-						'cat':
-							exceptionOccur(1, String(command + ' [file]'))
-						'cd':
-							exceptionOccur(1, String(command + ' [directory]'))
-						'run':
-							exceptionOccur(1, String(command + ' [file]'))
-						_:
-							exceptionOccur(0, command)
+					exceptionOccur(0, command)
+			
+			if exception != null:
+				exceptionOccur(1, String(command + exception))
 
-
+# Adds a new line to consoleLog bbcode
 func addToConsoleLog(isLog, command, br = '\n'):
 	var newline
 	
@@ -137,22 +142,17 @@ func addToConsoleLog(isLog, command, br = '\n'):
 	
 	consoleLog.bbcode_text += newline
 
-
+# Adds an error line to consoleLog bbcode
 func exceptionOccur(id, exception):
 	if id == 0:
 		addToConsoleLog(0, String('Command not found: ' + exception))
 	else:
 		addToConsoleLog(0, String('Syntax error: ' + exception))
 
-
+# Adds the introduced command to historyIndex array
 func addToHistory(command):
 	historyIndex += 1
 	commandHistory.insert(historyIndex, String(command))
-
-
-func printHistory():
-	for i in historyIndex + 1:
-		addToConsoleLog(0, String(i) + String(': ' + commandHistory[i]))
 
 
 func _on_anim_tween_completed(_object, _key):
@@ -165,3 +165,25 @@ func _on_console__input_text_entered(new_text):
 		addToConsoleLog(1, new_text, '\n\n')
 		validateCommand(new_text)
 		addToHistory(new_text)
+
+# Simple commands
+func clearTerminal():
+	consoleLog.bbcode_text = ''
+
+func printDate():
+	addToConsoleLog(0, transformDate())
+
+func showHelp():
+	addToConsoleLog(0, load_file('res://media/txt/help.txt'))
+
+
+func printHistory():
+	for i in historyIndex + 1:
+		addToConsoleLog(0, String(i) + String(': ' + commandHistory[i]))
+
+
+func listContent():
+	pass
+
+func printCurrentDir():
+	addToConsoleLog(0, currentDirectory)
